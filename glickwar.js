d0fcddd6e0a48d787d476c743dc9a2bf45d49e27
Dan Glick <danjglick@gmail.com>
2021-01-17 16:43:10 -0500

    clean up some names

--- glickwar.js
+++ glickwar.js
@@ -15,16 +15,16 @@ let game_deck = [
 ]
 let user_cards_left = []
 let opponent_cards_left = []
-let user_played_cards = []
-let opponent_played_cards = []
+let user_hand = []
+let opponent_hand = []
 let number_of_user_bets_this_round = 0
 let number_of_opponent_bets_this_round = 0
-let opponent_ai_is_on = false
+let is_artificial_intelligence_on = false
 const OPACITY_LEVEL_FOR_GREYED_OUT_CARD = 0.4
 
-function toggle_opponent_ai() {
+function toggle_artificial_intelligence() {
     document.getElementById("network_gif").hidden = !document.getElementById("network_gif").hidden
-    opponent_ai_is_on = !opponent_ai_is_on
+    is_artificial_intelligence_on = !is_artificial_intelligence_on
 }
 
 function start_new_game() {
@@ -51,13 +51,13 @@ function start_new_game() {
     document.getElementById("user_cards_left").innerHTML = user_cards_left.length.toString()
     document.getElementById("opponent_cards_left").innerHTML = opponent_cards_left.length.toString()
     document.getElementById("play_next_hand_button").disabled = false
-    document.getElementById("toggle_opponent_ai_button").disabled = false
+    document.getElementById("toggle_artificial_intelligence_button").disabled = false
     document.getElementById("celebration_gif").hidden = true
     document.getElementById("embarrassed_gif").hidden = true
 }
 
 async function play_next_hand() {
-    if (user_played_cards.length > 0) {
+    if (user_hand.length > 0) {
         _handle_hand_winner()
     }
     console.log([user_cards_left, opponent_cards_left])
@@ -95,15 +95,15 @@ function _deal_26_cards_to(player_deck) {
 function _play_user_card() {
     if (user_cards_left.length > 0) {
         let user_card = user_cards_left.pop()
-        user_played_cards.push(user_card)
+        user_hand.push(user_card)
         let card_element = document.createElement("IMG")
         card_element.className = "played_card played_user_card"
         card_element.src = `images/${user_card}.png`
         card_element.width = 150
         card_element.height = 225
-        document.getElementById("user_played_cards").appendChild(card_element)
+        document.getElementById("user_hand").appendChild(card_element)
         document.getElementById("user_cards_left").innerHTML = user_cards_left.length.toString()
-        if (opponent_ai_is_on === true) {
+        if (is_artificial_intelligence_on === true) {
             _run_opponent_ai()
         }
     }
@@ -112,20 +112,20 @@ function _play_user_card() {
 function _play_opponent_card() {
     if (opponent_cards_left.length > 0) {
         let opponent_card = opponent_cards_left.pop()
-        opponent_played_cards.push(opponent_card)
+        opponent_hand.push(opponent_card)
         let card_element = document.createElement("IMG")
         card_element.className = "played_card played_opponent_card"
         card_element.src = `images/${opponent_card}.png`
         card_element.width = 150
         card_element.height = 225
-        document.getElementById("opponent_played_cards").appendChild(card_element)
+        document.getElementById("opponent_hand").appendChild(card_element)
         document.getElementById("opponent_cards_left").innerHTML = opponent_cards_left.length.toString()
     }
 }
 
 async function _run_opponent_ai() {
     for (let i = 0; i < 3; i++) {
-        if (number_of_opponent_bets_this_round < 3 && _get_last_played_value(user_played_cards) > _get_last_played_value(opponent_played_cards) && _get_last_played_value(user_played_cards) < 11) {
+        if (number_of_opponent_bets_this_round < 3 && _get_last_played_value(user_hand) > _get_last_played_value(opponent_hand) && _get_last_played_value(user_hand) < 11) {
             await new Promise(r => setTimeout(r, 1000))
             _bet_opponent_card()
         }
@@ -145,8 +145,8 @@ function _get_last_played_value(player_played_cards) {
 }
 
 function _clear_played_cards() {
-    user_played_cards = []
-    opponent_played_cards = []
+    user_hand = []
+    opponent_hand = []
     let played_cards_elements = document.getElementsByClassName("played_card")
     while (played_cards_elements.length > 0) {
         played_cards_elements[0].parentNode.removeChild(played_cards_elements[0])
@@ -154,36 +154,36 @@ function _clear_played_cards() {
 }
 
 function _handle_hand_winner() {
-    let user_last_played_value = _get_last_played_value(user_played_cards)
-    let opponent_last_played_value = _get_last_played_value(opponent_played_cards)
+    let user_last_played_value = _get_last_played_value(user_hand)
+    let opponent_last_played_value = _get_last_played_value(opponent_hand)
     if (user_last_played_value > opponent_last_played_value) {
-        for (let i = 0; i < user_played_cards.length; i++) {
-            user_cards_left.unshift(user_played_cards[i])
+        for (let i = 0; i < user_hand.length; i++) {
+            user_cards_left.unshift(user_hand[i])
         }
-        for (let i = 0; i < opponent_played_cards.length; i++) {
-            user_cards_left.unshift(opponent_played_cards[i])
+        for (let i = 0; i < opponent_hand.length; i++) {
+            user_cards_left.unshift(opponent_hand[i])
         }
         _emphasize_updated_user_cards_left();
     } else if (user_last_played_value < opponent_last_played_value) {
-        for (let i = 0; i < user_played_cards.length; i++) {
-            opponent_cards_left.unshift(user_played_cards[i])
+        for (let i = 0; i < user_hand.length; i++) {
+            opponent_cards_left.unshift(user_hand[i])
         }
-        for (let i = 0; i < opponent_played_cards.length; i++) {
-            opponent_cards_left.unshift(opponent_played_cards[i])
+        for (let i = 0; i < opponent_hand.length; i++) {
+            opponent_cards_left.unshift(opponent_hand[i])
         }
         _emphasize_updated_opponent_cards_left()
-    } else if (user_played_cards.length > 0) {
-        for (let i = 0; i < user_played_cards.length; i++) {
-            user_cards_left.unshift(user_played_cards[i])
+    } else if (user_hand.length > 0) {
+        for (let i = 0; i < user_hand.length; i++) {
+            user_cards_left.unshift(user_hand[i])
         }
-        for (let i = 0; i < opponent_played_cards.length; i++) {
-            opponent_cards_left.unshift(opponent_played_cards[i])
+        for (let i = 0; i < opponent_hand.length; i++) {
+            opponent_cards_left.unshift(opponent_hand[i])
         }
     }
 }
 
 async function _emphasize_updated_user_cards_left() {
-    document.getElementById("user_cards_left").innerHTML = `+${opponent_played_cards.length}`
+    document.getElementById("user_cards_left").innerHTML = `+${opponent_hand.length}`
     document.getElementById("user_cards_left").style.fontSize = "48"
     document.getElementById("user_cards_left").style.color = "green"
     await new Promise(r => setTimeout(r, 1000))
@@ -193,7 +193,7 @@ async function _emphasize_updated_user_cards_left() {
 }
 
 async function _emphasize_updated_opponent_cards_left() {
-    document.getElementById("opponent_cards_left").innerHTML = `+${user_played_cards.length}`
+    document.getElementById("opponent_cards_left").innerHTML = `+${user_hand.length}`
     document.getElementById("opponent_cards_left").style.fontSize = "48"
     document.getElementById("opponent_cards_left").style.color = "green"
     await new Promise(r => setTimeout(r, 1000))
@@ -212,5 +212,5 @@ function _handle_game_winner() {
     }
     document.getElementById("play_next_hand_button").disabled = true
     document.getElementById("bet_another_card_button").disabled = true
-    document.getElementById("toggle_opponent_ai_button").disabled = true
+    document.getElementById("toggle_artificial_intelligence_button").disabled = true
 }
